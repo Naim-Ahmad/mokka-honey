f376b8bb64995bda6e11dda2e26ea60118cbb10e
Naim Ahmad <naimahmad201103@gmail.com>
2024-01-03 09:50:30 +0600

    login form created

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,23 +1,19 @@
-import React from 'react'
-import headerImage from '../../Assets/bannar-min.jpg'
-import HeaderAll from '../../Components/HeaderAll'
-import AboutData from '../../Data/AboutData'
-import AboutBody from './AboutBody'
+import React from "react";
+import headerImage from "../../Assets/bannar-min.jpg";
+import HeaderAll from "../../Components/HeaderAll";
+import AboutData from "../../Data/AboutData";
+import AboutBody from "./AboutBody";
 
 export default function About() {
   return (
     <>
-       <HeaderAll imageUrl={headerImage}>
-          আমাদের পরিচয়
-        </HeaderAll>
+      <HeaderAll imageUrl={headerImage}>আমাদের পরিচয়</HeaderAll>
 
-       <div className='container'>
-
-       {AboutData.map(about=>{
-        return <AboutBody key={about.id} {...about}/>
-       })}
-       
-       </div>
+      <div className="container">
+        {AboutData.map((about) => {
+          return <AboutBody key={about.id} {...about} />;
+        })}
+      </div>
     </>
-  )
+  );
 }
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from "react";
+import { Form } from "react-bootstrap";
+import headerImage from "../../Assets/bannar-min.jpg";
+import HeaderAll from "../../Components/HeaderAll";
 
 export default function Login() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userEmail = e.target.userEmail.value;
+    const userPassword = e.target.userPassword.value;
+
+    const userInfo = {
+      userEmail,
+      userPassword,
+    };
+    console.log(userInfo);
+  };
+  
   return (
-    <div>Login</div>
-  )
+    <>
+      <HeaderAll imageUrl={headerImage}>দয়া করে লগইন করুন</HeaderAll>
+      <div className="container">
+        <div className="w-50 mx-auto py-5 px-4 my-4 bg-light rounded">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label className="fw-bold">ই-মেইল এড্রেস</Form.Label>
+              <Form.Control
+                type="email"
+                name="userEmail"
+                placeholder="test@example.com"
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label className="fw-bold">পাসওয়ার্ড</Form.Label>
+              <Form.Control
+                type="password"
+                name="userPassword"
+                placeholder="******"
+              />
+            </Form.Group>
+
+            <button type="submit" className="my-btn w-100">
+              লগইন
+            </button>
+          </Form>
+          <hr className="bg-primary text-primary my-4" />
+          <div>
+            <button className="btn btn-outline-warning text-black w-100 rounded-pill fw-bold">
+              <i className="fa-brands mx-2 text-black fa-google"></i>গুগল দিয়ে
+              লগইন করুন
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
